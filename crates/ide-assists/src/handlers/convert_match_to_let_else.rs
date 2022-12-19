a275168c40bf964b3ee1c46be8dc9f8e5f98df79
Liam Hession <liam@gobootler.com>
2022-12-19 13:05:31 -0500

    Got all the tests passing with very simplified approach. Cleanup, commenting, context-adding to come

--- crates/ide-assists/src/handlers/convert_match_to_let_else.rs
+++ crates/ide-assists/src/handlers/convert_match_to_let_else.rs
@@ -1,50 +1,29 @@
-// 9:52 - 
-// next up: trying further work to bring over binders_in_pat from convert_let_else_to_match 
-//     and: switching to master branch and rebuilding the whole thing, can i run the original version of this test in debug mode to step through and follow how it works?
+// 12:15 - 
+// next up: TODO1 = try getting the tuple test towards the end of the test suite to work, by actually iterating through binders - need to make it a rename_variables function
+//      your new superpower is not being afraid of using parent() and child() syntax tree functions
+
 use ide_db::defs::{Definition, NameRefClass};
+use hir::Semantics;
+use ide_db::RootDatabase;
 use syntax::{
-    ast::{self, HasName},
+    ast::{self, HasName, Name, Pat},
     ted, AstNode, SyntaxNode,
 };
 
-use crate::{
-    assist_context::{AssistContext, Assists},
-    AssistId, AssistKind,
-};
+use crate::{AssistContext, AssistId, AssistKind, Assists};
 
-// Given a pattern, find the name of the identifier introduced to the surrounding scope.
-fn find_simple_binding(pat: ast::Pat) -> Option<ast::IdentPat> {
-    if let ast::Pat::IdentPat(ident) = pat {
-        Some(ident)
-    } else {
-        None
-    }
-}
+// // Given a pattern, find the name of the identifier introduced to the surrounding scope.
+// fn find_simple_binding(pat: ast::Pat) -> Option<ast::IdentPat> {
+//     if let ast::Pat::IdentPat(ident) = pat {
+//         Some(ident)
+//     } else {
+//         None
+//     }
+// }
 
-// Tuple version
-fn find_tuple_binding(pat: ast::Pat) -> Option<ast::TuplePat> {
-    if let ast::Pat::TuplePat(tuple) = pat {
-        Some(tuple)
-    } else {
-        None
-    }
-}
-
-// Unified version, just returns a Pat no matter what
-// Next time - get any idea of how better to return different Pat types
-// in addition to ast::IdentPat it should also find ast::TuplePat and other patterns that makes sense
-fn find_binding(pat: ast::Pat) -> Option<ast::Pat> {
-    if let ast::Pat::IdentPat(ident) = pat {
-        Some(ident)
-    } else if let ast::Pat::TuplePat(tuple) = pat {
-        Some(tuple)
-    } else {
-        None
-    }
-}
-
+// TO CHECK: can actually get rid of the "is_mutable" boolean, if going to use peek at "parent()" each time
 fn binders_in_pat(
-    acc: &mut Vec<(Name, bool)>,
+    acc: &mut Vec<Name>, // binding Identifier Pattern, and whether it is mut
     pat: &Pat,
     sem: &Semantics<'_, RootDatabase>,
 ) -> Option<()> {
@@ -55,13 +34,61 @@ fn binders_in_pat(
             let ismut = p.ref_token().is_none() && p.mut_token().is_some();
             // check for const reference
             if sem.resolve_bind_pat_to_const(p).is_none() {
-                acc.push((ident, ismut));
+                acc.push(ident);
             }
             if let Some(inner) = p.pat() {
                 binders_in_pat(acc, &inner, sem)?;
             }
             Some(())
         }
+        BoxPat(p) => p.pat().and_then(|p| binders_in_pat(acc, &p, sem)),
+        RestPat(_) | LiteralPat(_) | PathPat(_) | WildcardPat(_) | ConstBlockPat(_) => Some(()),
+        OrPat(p) => {
+            for p in p.pats() {
+                binders_in_pat(acc, &p, sem)?;
+            }
+            Some(())
+        }
+        ParenPat(p) => p.pat().and_then(|p| binders_in_pat(acc, &p, sem)),
+        RangePat(p) => {
+            if let Some(st) = p.start() {
+                binders_in_pat(acc, &st, sem)?
+            }
+            if let Some(ed) = p.end() {
+                binders_in_pat(acc, &ed, sem)?
+            }
+            Some(())
+        }
+        RecordPat(p) => {
+            for f in p.record_pat_field_list()?.fields() {
+                let pat = f.pat()?;
+                binders_in_pat(acc, &pat, sem)?;
+            }
+            Some(())
+        }
+        RefPat(p) => dbg!(p.pat().and_then(|p| binders_in_pat(acc, &p, sem))),
+        SlicePat(p) => {
+            for p in p.pats() {
+                binders_in_pat(acc, &p, sem)?;
+            }
+            Some(())
+        }
+        TuplePat(p) => {
+            for p in p.fields() {
+                binders_in_pat(acc, &p, sem)?;
+            }
+            Some(())
+        }
+        TupleStructPat(p) => {
+            dbg!(p);
+            for p in p.fields() {
+                binders_in_pat(acc, &p, sem)?;
+            }
+            Some(())
+        }
+        // don't support macro pat yet
+        MacroPat(_) => None,
+    }
 }
 
 // Assist: convert_match_to_let_else
@@ -86,46 +113,57 @@ fn binders_in_pat(
 pub(crate) fn convert_match_to_let_else(acc: &mut Assists, ctx: &AssistContext<'_>) -> Option<()> {
     let let_stmt: ast::LetStmt = ctx.find_node_at_offset()?;
 
-    let pat = let_stmt.pat()?;
-    println!("{}", pat);
-    let mut binders = Vec::new();
-    binders_in_pat(&mut binders, &pat, &ctx.sema);
-    // let binding =
-    // match find_simple_binding(pat) {
-    //     Some(simple_identifier) => simple_identifier,
-    //     None => find_tuple_binding(pat),
-    // };
-    
+    let top_level_pat = let_stmt.pat()?;
+    println!("let_stmt.pat: {top_level_pat}");
 
-    // initializer is the expression whose value will be assigned to the identifer/tuple of identifiers on the lhs
-    let initializer = match let_stmt.initializer() {
+    // initializer must be a MatchExpr, or this Assist does not apply
+    let initializer_raw = let_stmt.initializer()?;
+    println!("let_stmt.initializer(): {initializer_raw}");
+
+    let initializer_match = match let_stmt.initializer() {
         Some(ast::Expr::MatchExpr(it)) => it,
-        _ => return None, // TO TEST: if there is no match statement actually, just let v = 5; it gets in here?
+        _ => return None,
     };
-    let initializer_expr = initializer.expr()?;  // TO TEST: and then this fails, ending the function?
+    let scrutinee_expr = initializer_match.expr()?;
+    println!("scrutinee expression of match, let_stmt.initializer().expr(): {scrutinee_expr}");
 
-    let (extracting_arm, diverging_arm) = match find_arms(ctx, &initializer) {
+    let (extracting_arm, diverging_arm) = match find_arms(ctx, &initializer_match) {
         Some(it) => it,
         None => return None,
     };
+    // The assist does not work for matches with guards
     if extracting_arm.guard().is_some() {
-        cov_mark::hit!(extracting_arm_has_guard); // TO TEST: Does not work for matches with guards?
+        cov_mark::hit!(extracting_arm_has_guard);
         return None;
     }
 
-    let diverging_arm_expr = diverging_arm.expr()?;  // None => return
-    let extracting_arm_pat = extracting_arm.pat()?;   // Some(it) => it
+    let diverging_arm_expr = diverging_arm.expr()?;  // return
+    let extracting_arm_pat = extracting_arm.pat()?;   // Some(it)
     let extracted_variable = find_extracted_variable(ctx, &extracting_arm)?;  // it
+    println!("diverging_arm_expr: {diverging_arm_expr}");
+    println!("extracting_arm_pat: {extracting_arm_pat}");
+    println!("extracted_variable: {extracted_variable}");
+
+    // let mut binders = Vec::new();
+    // binders_in_pat(&mut binders, &top_level_pat, &ctx.sema);
+
+    // // let bindings = binders;
+    // dbg!(binders);
+
+    // let binding = find_simple_binding(pat)?;
+
+    let target = let_stmt.syntax().text_range();
+
+    let pat_in_extracting_arm_format = rename_variables(&extracting_arm_pat, extracted_variable, top_level_pat); //&binders);
 
     acc.add(
         AssistId("convert_match_to_let_else", AssistKind::RefactorRewrite),
         "Convert match to let-else",
-        let_stmt.syntax().text_range(),
+        target,
         |builder| {
-            let extracting_arm_pat = rename_variable(&extracting_arm_pat, extracted_variable, binding);  // Some(val)
             builder.replace(
-                let_stmt.syntax().text_range(),
-                format!("let {extracting_arm_pat} = {initializer_expr} else {{ {diverging_arm_expr} }};")
+                target,
+                format!("let {pat_in_extracting_arm_format} = {scrutinee_expr} else {{ {diverging_arm_expr} }};")
             )
         },
     )
@@ -163,7 +201,7 @@ fn find_arms(
 
 // Given an extracting arm, find the extracted variable.
 // should probably return Vec<ast::Name> for every binding? instead of a single name
-fn find_extracted_variable(ctx: &AssistContext<'_>, arm: &ast::MatchArm) -> Option<ast::Name> {
+fn find_extracted_variable(ctx: &AssistContext<'_>, arm: &ast::MatchArm) -> Option<Name> {
     match arm.expr()? {
         ast::Expr::PathExpr(path) => {
             let name_ref = path.syntax().descendants().find_map(ast::NameRef::cast)?;
@@ -182,12 +220,38 @@ fn find_extracted_variable(ctx: &AssistContext<'_>, arm: &ast::MatchArm) -> Opti
     }
 }
 
-// Rename `extracted` with `binding` in `pat`.
-// example: pat:= Some(it) => it     extracted:= it    binding:=  val
-// should be updated according to return types of previous functions and should handle new cases
-fn rename_variable(pat: &ast::Pat, extracted: ast::Name, binding: ast::IdentPat) -> SyntaxNode {
+// // Rename `extracted` with `binding` in `pat`.
+// // example: pat:= Some(it) => it     extracted:= it    binding:=  val
+// // should be updated according to return types of previous functions and should handle new cases
+// fn rename_variable(pat: &ast::Pat, extracted: Name, binding: ast::IdentPat) -> SyntaxNode {
+//     let syntax = pat.syntax().clone_for_update();
+//     let extracted_syntax = syntax.covering_element(extracted.syntax().text_range());
+
+//     // If `extracted` variable is a record field, we should rename it to `binding`,
+//     // otherwise we just need to replace `extracted` with `binding`.
+
+//     // if let Some(record_pat_field) = extracted_syntax.ancestors().find_map(ast::RecordPatField::cast)
+//     // {
+//     //     if let Some(name_ref) = record_pat_field.field_name() {
+//     //         ted::replace(
+//     //             record_pat_field.syntax(),
+//     //             ast::make::record_pat_field(ast::make::name_ref(&name_ref.text()), binding.into())
+//     //                 .syntax()
+//     //                 .clone_for_update(),
+//     //         );
+//     //     }
+//     // } else {
+//         ted::replace(extracted_syntax, binding.syntax().clone_for_update());
+//     // }
+
+//     syntax
+// }
+
+fn rename_variables(pat: &ast::Pat, extracted: Name, let_stmt_pat: Pat) -> SyntaxNode {
     let syntax = pat.syntax().clone_for_update();
+    dbg!(extracted.syntax().parent());
     let extracted_syntax = syntax.covering_element(extracted.syntax().text_range());
+    println!("syntax: {syntax}, extracted_syntax: {extracted_syntax}");
 
     // If `extracted` variable is a record field, we should rename it to `binding`,
     // otherwise we just need to replace `extracted` with `binding`.
@@ -197,16 +261,30 @@ fn rename_variable(pat: &ast::Pat, extracted: ast::Name, binding: ast::IdentPat)
         if let Some(name_ref) = record_pat_field.field_name() {
             ted::replace(
                 record_pat_field.syntax(),
-                ast::make::record_pat_field(ast::make::name_ref(&name_ref.text()), binding.into())
+                ast::make::record_pat_field(ast::make::name_ref(&name_ref.text()), let_stmt_pat.into())
                     .syntax()
                     .clone_for_update(),
             );
         }
     } else {
-        ted::replace(extracted_syntax, binding.syntax().clone_for_update());
+    // } else {
+        // let (binding_name, is_binding_mut) = binding_and_mutability;
+        // let bindings_str = if *is_binding_mut { format!("mut {binding_name}") } else { format!("{binding_name}") }; 
+        // parent of binding is the node that includes "ref" and "mut" if it had them
+        // TODO1: will need to iterate through extracted as well when it's a tuple, and put each binder in the place of its corresponding extracted thing
+        // for (binder) in binders.iter() {
+        //     let binding_with_modifiers = match binder.syntax().parent() {
+        //         Some(parent_ident) => parent_ident,
+        //         None => binder.syntax().to_owned(),
+        //     };
+        //     ted::replace(extracted_syntax, binding_with_modifiers.clone_for_update()); // is this necessary, what other unnecessary deref stuff is in preceding lines?
+        // }
+    // }
+
+        ted::replace(extracted_syntax, let_stmt_pat.syntax().clone_for_update()); // is this necessary, what other unnecessary deref stuff is in preceding lines?
     }
 
-    syntax
+    syntax //.text().to_string().replace(&extracted_syntax.to_string(), &dbg!(bindings_str))
 }
 
 #[cfg(test)]
@@ -214,6 +292,19 @@ mod tests {
     use crate::tests::{check_assist, check_assist_not_applicable};
 
     use super::*;
+
+    #[test]
+    fn should_not_be_applicable_for_non_match_initializer() {
+        check_assist_not_applicable(
+            convert_match_to_let_else,
+            r#"
+//- minicore: option
+fn foo(opt: Option<()>) {
+    let val = $05;
+}
+"#,
+        );
+    }
 
     #[test]
     fn should_not_be_applicable_for_non_diverging_match() {
@@ -419,7 +510,7 @@ fn foo(opt: Option<()>) {
     }
 
     #[test]
-    fn struct_pattern() {
+    fn struct_pattern() {   // FAILS - was this covered by the "RecordPatField" case in the original?
         check_assist(
             convert_match_to_let_else,
             r#"
@@ -456,7 +547,7 @@ fn foo(opt: Option<Point>) {
             r#"
 //- minicore: option
 fn foo() {
-    let (val1, val2) = $0match Some((0, 1)) {
+    let (x, y) = $0match Some((0, 1)) {
         Some(it) => it,
         None => return,
     };
